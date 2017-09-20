c483e852fad27172e4a508f57bca49df3c1f786d
TitanSnow <sweeto@live.cn>
2017-09-20 09:12:42 +0800

    add more info in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,18 @@ setup(
     url='https://github.com/TitanSnow/luap',
     description='A lua REPL written in python',
     long_description=open('README.rst').read(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Other Scripting Engines',
+        'Topic :: Software Development :: Interpreters',
+    ],
     packages=('luap',),
     install_requires=(
         'prompt_toolkit>=1.0.4, <2',
